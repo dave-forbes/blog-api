73686caacdb71f7a1a4d07a0ca98a98855833e76
David Forbes <davidforbesi@hotmail.com>
2024-02-14 12:56:13 +0000

    updated comment model to incude relavent post

--- src/models/commentModel.ts
+++ src/models/commentModel.ts
@@ -6,6 +6,7 @@ const CommentSchema = new Schema(
   {
     text: { type: String, required: true, maxlength: 250 },
     user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    post: { type: Schema.Types.ObjectId, ref: "Post", required: true },
   },
   { timestamps: true }
 );
