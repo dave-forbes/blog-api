639d81a6c6053440d87d16abaad64bf977914923
David Forbes <davidforbesi@hotmail.com>
2024-02-14 10:36:26 +0000

    import and use routes

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
+import userRouter from "./routes/userRoutes";
+import postRouter from "./routes/postRoutes";
+import commentRouter from "./routes/commentRoutes";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use("/users", userRouter);
+app.use("/posts", postRouter);
+app.use("/comments", commentRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
