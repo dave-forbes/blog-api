820ac686e98ec6e11d07b4dfb88b81509b31a507
David Forbes <davidforbesi@hotmail.com>
2024-02-14 12:22:12 +0000

    add middleware for parsing incoming request bodies and database connection

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,27 @@ import dotenv from "dotenv";
 import userRouter from "./routes/userRoutes";
 import postRouter from "./routes/postRoutes";
 import commentRouter from "./routes/commentRoutes";
+import connect from "./database";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+// database connection
+
+connect();
+
+// middleware
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// routes
+
 app.use("/users", userRouter);
 app.use("/posts", postRouter);
 app.use("/comments", commentRouter);
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
 
 // Error Handling Middleware
 app.use(
