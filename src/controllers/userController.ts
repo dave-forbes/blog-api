9b6911fdf68b81428a718a282303dbca223b4733
David Forbes <davidforbesi@hotmail.com>
2024-02-14 11:53:21 +0000

    create logic for registering a new user

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,9 +1,53 @@
 import express from "express";
 import { body, validationResult } from "express-validator";
+import bcrypt from "bcrypt";
+import User from "../models/userModel";
 
 // Register a new user
 
-const registerUser = (req: express.Request, res: express.Response) => {};
+const registerUser = [
+  body("username")
+    .trim()
+    .isLength({ min: 1 })
+    .withMessage("Username is required"),
+  body("password")
+    .isLength({ min: 8 })
+    .withMessage("Password must be at least 8 characters long"),
+  body("confirmPassword").custom((value, { req }) => {
+    if (value !== req.body.password) {
+      throw new Error("Passwords do not match");
+    }
+    return true;
+  }),
+  async (
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) => {
+    try {
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+
+      const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+      const newUser = new User({
+        username: req.body.username,
+        password: hashedPassword,
+      });
+
+      await newUser.save();
+
+      res
+        .status(201)
+        .json({ message: `New user, ${newUser.username}, created` });
+    } catch (error) {
+      next(error);
+    }
+  },
+];
 
 // Login user
 
@@ -17,6 +61,6 @@ const logoutUser = (req: express.Request, res: express.Response) => {};
 
 const getUser = () => {};
 
-const AuthController = { registerUser, loginUser, logoutUser, getUser };
+const UserController = { registerUser, loginUser, logoutUser, getUser };
 
-export default AuthController;
+export default UserController;
