f7bf7385cc7745a3783d95760bcb07fb62683063
David Forbes <davidforbesi@hotmail.com>
2024-02-14 12:53:18 +0000

    added logic for creating a post

--- src/controllers/postController.ts
+++ src/controllers/postController.ts
@@ -1,6 +1,45 @@
+import { NextFunction, Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+import Post from "../models/postModel";
+
 // create post
 
-const createPost = () => {};
+const createPost = [
+  body("title")
+    .trim()
+    .isLength({ min: 1 })
+    .withMessage("Title is required")
+    .customSanitizer((value) => {
+      return value.replace(/[^a-zA-Z0-9\s\_\-']/g, "");
+    }),
+  body("text")
+    .trim()
+    .customSanitizer((value) => {
+      return value.replace(/[^a-zA-Z0-9\s\_\-']/g, "");
+    })
+    .isLength({ min: 1 })
+    .withMessage("Text is required"),
+
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const errors = validationResult(req);
+
+      const newPost = new Post({
+        title: req.body.title,
+        text: req.body.text,
+        user: req.body.user,
+      });
+
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ newPost, errors: errors.array() });
+      }
+      await newPost.save();
+      res.status(201).json({ newPost, message: "New post created" });
+    } catch (error) {
+      next(error);
+    }
+  },
+];
 
 // read post
 
