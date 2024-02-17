65bfb9fcf25e44bbb92581e0993eae7a26b568aa
David Forbes <davidforbesi@hotmail.com>
2024-02-17 11:20:30 +0000

    install and enable cors

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import userRouter from "./routes/userRoutes";
 import postRouter from "./routes/postRoutes";
 import commentRouter from "./routes/commentRoutes";
 import connect from "./utils/database";
+import cors from "cors";
 
 dotenv.config();
 
@@ -18,6 +19,7 @@ connect();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 // routes
 
