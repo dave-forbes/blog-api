262b305fe7e443aca03232b68127c40bc7a7353b
David Forbes <davidforbesi@hotmail.com>
2024-02-21 16:36:47 +0000

    change interface and variable name to avoid confusion, also add author property to type

--- src/utils/generateAccessToken.ts
+++ src/utils/generateAccessToken.ts
@@ -1,16 +1,19 @@
 import jwt from "jsonwebtoken";
 import { Types } from "mongoose";
 
-interface User {
+interface TokenRecipient {
   username: string;
   id: Types.ObjectId;
+  author: Boolean;
 }
 
-const generateAccessToken = (user: User): string | Error => {
+const generateAccessToken = (
+  tokenRecipient: TokenRecipient
+): string | Error => {
   if (!process.env.JWT_SECRET) {
     throw new Error("JWT_SECRET not found in environment variables");
   }
-  return jwt.sign(user, process.env.JWT_SECRET, { expiresIn: "1h" });
+  return jwt.sign(tokenRecipient, process.env.JWT_SECRET, { expiresIn: "1h" });
 };
 
 export default generateAccessToken;
