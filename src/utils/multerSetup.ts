2fc7f54e220059ce1eaac3f18a29b02b08f4bc64
David Forbes <davidforbesi@hotmail.com>
2024-02-26 10:52:08 +0000

    fix typo in diskStorage file destination

--- src/utils/multerSetup.ts
+++ src/utils/multerSetup.ts
@@ -5,7 +5,7 @@ const parentDir = path.resolve(__dirname, "../..");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.join(parentDir, "public/images"));
+    cb(null, path.join(parentDir, "public/img"));
   },
   filename: function (req, file, cb) {
     cb(null, `${Date.now()}--${file.originalname}`);
