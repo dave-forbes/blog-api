b7205f0158262fc57c44c160ec90d0336c0916b0
David Forbes <davidforbesi@hotmail.com>
2024-02-26 10:40:56 +0000

    increase payload limit

--- src/index.ts
+++ src/index.ts
@@ -17,8 +17,8 @@ connect();
 
 // middleware
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: "3mb" }));
+app.use(express.urlencoded({ limit: "3mb", extended: true }));
 app.use(cors());
 app.use(express.static("public"));
 
