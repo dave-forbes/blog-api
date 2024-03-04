bcadb58abfa11b75c3b1626ed9bb7af9ddeae8f4
David Forbes <davidforbesi@hotmail.com>
2024-03-04 16:24:32 +0000

    remove unessesary console.log

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -26,7 +26,6 @@ const registerUser = [
       const errors = validationResult(req);
 
       if (!errors.isEmpty()) {
-        console.log(errors.array());
         return res.status(400).json({ errors: errors.array() });
       }
 
