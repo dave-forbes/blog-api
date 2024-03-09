22fd0ad69ff3a4ee1af45d9c4f6503c3c8242820
David Forbes <davidforbesi@hotmail.com>
2024-03-09 12:37:04 +0000

    import and implement mongo memory server
    - change connect function to connectDB for clarity
    - add logic to connect to test db id in testing environment
    - add a function to disconnect from db
    - change exports to named exports

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,20 +1,38 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import { MongoMemoryServer } from "mongodb-memory-server";
+
+let mongoServer: MongoMemoryServer | null = null;
 
 dotenv.config();
 
-const connect = async () => {
+const connectDB = async (isTesting: boolean = false) => {
   try {
-    if (!process.env.MONGODB_URI) {
-      console.error("MONGODB_URI not found in environment variables");
-      process.exit(1);
+    if (isTesting) {
+      mongoServer = new MongoMemoryServer();
+      await mongoServer.start(); // Start the server before getting the URI
+      const mongoUri = await mongoServer.getUri();
+      await mongoose.connect(mongoUri);
+      console.log("Connected to MongoDB for testing");
+    } else {
+      if (!process.env.MONGODB_URI) {
+        console.error("MONGODB_URI not found in environment variables");
+        process.exit(1);
+      }
+      await mongoose.connect(process.env.MONGODB_URI);
+      console.log("Connected to MongoDB");
     }
-    await mongoose.connect(process.env.MONGODB_URI);
-    console.log("Connected to MongoDB");
   } catch (error: any) {
     console.error("Error connecting to MongoDB:", error.message);
     process.exit(1); // Exit the process if unable to connect to the database
   }
 };
 
-export default connect;
+const disconnectDB = async () => {
+  await mongoose.disconnect();
+  if (mongoServer) {
+    await mongoServer.stop();
+  }
+};
+
+export { connectDB, disconnectDB };
